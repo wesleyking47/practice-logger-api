23457fb22da3ba66cf81507340b23a6f3eb14ef1
Wesley King <wesley.king@leadingedje.com>
2026-01-30 09:49:31 -0500

    adds deploy pipeline

--- src/PracticeLogger.Api/Program.cs
+++ src/PracticeLogger.Api/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
 builder.Services.AddHealthChecks();
 
 builder.Services.AddOpenApi();
